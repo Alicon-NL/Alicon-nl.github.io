43aff8729c41619add6e99aec549d8269330fcc3
Max van Kampen <max.van.kampen@alicon.nl>
2021-05-21 10:17:10 +0200

    Auto commit

---
+++ js/config.js
@@ -0,0 +1,22 @@
+$().config({
+  client_id: "2347321-a0bdd840-9d16-403c-a578-cafd21f112e5",
+  info: {
+    title: "Aliconnect",
+    description: "One Source, One Truth",
+    version: "1.0.0",
+    contact: {
+      email: "max.van.kampen@alicon.nl"
+    },
+    license: {
+      name: "Apache 2.0",
+      url: "http://www.apache.org/licenses/LICENSE-2.0.html"
+    }
+  },
+  client: {
+    servers: [
+      {
+        url: "https://alicon.aliconnect.nl/api"
+      }
+    ]
+  },
+});
